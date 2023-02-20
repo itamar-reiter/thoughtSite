3306ea4affd20319b87d9ad783d9143a25cdb1be
itamar reiter <itamarreiter@gmail.com>
2023-02-20 15:12:41 +0200

    create header

--- thoughtsite/src/components/App/App.js
+++ thoughtsite/src/components/App/App.js
@@ -1,15 +1,16 @@
 import React from 'react'
+import './App.css';
 import Header from '../Header/Header';
 import Main from '../Main/Main';
 import Footer from '../Footer/Footer';
 
 function App() {
   return (
-    <>
+    <div className='app'>
       <Header />
       <Main />
       <Footer />
-    </>
+    </div>
   )
 }
 
--- thoughtsite/src/components/Navigation/Navigation.js
+++ thoughtsite/src/components/Navigation/Navigation.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import './Navigation.css';
+/* import { Link } from "react-router-dom"; */
 
 function Navigation() {
   return (
-    <div>Navigation</div>
+    <nav className='navigation'>
+      {/* <Link to='/' className='navigation__link'>Home</Link>
+      <Link to='/search' className='navigation__link'>search</Link>
+      <Link to='/freinds' className='navigation__link'>friends</Link>
+      <Link to='/login' className='navigation__link'>log in</Link> */}
+      <button className='navigation__button'>log out</button>
+      <button className='navigation__button'>log out</button>
+      <button className='navigation__button'>log out</button>
+      <button className='navigation__button'>log out</button>
+    </nav>
   )
 }
 
